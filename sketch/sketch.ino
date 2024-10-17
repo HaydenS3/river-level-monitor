ff2c65fc5c79404a0f00081df938d33ce3eac338
Hayden Schroeder <hschroeder0317@gmail.com>
2024-10-17 12:28:39 -0500

    Sketch

---
+++ sketch/sketch.ino
@@ -0,0 +1,12 @@
+// Authors: Hayden Schroeder and Erica Shi
+// Description: Take data from sensors and store it locally as well as have web access to the data.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
